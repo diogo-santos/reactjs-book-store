3ac538f9a3e6f9cc8351b5e2bea8ee436dcfef31
diogo-santos <diogoffs@outlook.com>
2020-09-07 17:15:51 +0100

    Small adjustment on view book info

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -1,21 +1,21 @@
 import React from "react";
+import moment from 'moment';
 
 function BookView(props) {
   return (
     <div className="d-flex flex-row">
-        <div className="p-2">
-          <img src={props.book.image || "http://via.placeholder.com/130x180"} alt=""/>
+      <div className="p-2">
+        <img src={props.book.image || "http://via.placeholder.com/130x180"} alt="" />
+      </div>
+      <div className="p-2">
+        <div>Author <span className="text-muted">{props.book.author}</span></div>
+        <div>Category <span className="text-muted">{props.book.category}</span></div>
+        <div>Publication Date 
+          <span className="text-muted ml-1">
+            {moment(props.book.publicationDate, "YYYY-MM-DD").format('DD/MM/YYYY')}
+          </span>
         </div>
-        <div className="p-2">
-            <div>Author</div>
-            <div>Category</div>
-            <div>Publication Date</div>
-        </div>
-        <div className="p-2">
-            <div>{props.book.author}</div>
-            <div>{props.book.category}</div>
-            <div>{props.book.publicationDate}</div>
-        </div>
+      </div>
     </div>
   );
 }
