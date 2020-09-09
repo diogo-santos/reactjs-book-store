568f7dedafef7d5396de7a3cb012de74222f718f
diogo-santos <diogoffs@outlook.com>
2020-09-09 23:30:41 +0100

    Display alert only if  message is not empty

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function Alert(props) {
   return (
-    <div className={`alert alert-${props.type} alert-dismissible fade show mt-2 mb-2`} role="alert">
-      {props.message}
-      <button type="button" className="close" onClick={() => props.onClose()}>
-        <span aria-hidden="true">&times;</span>
-      </button>
-  </div>
+    props.message && (
+      <div className={`alert alert-${props.type} alert-dismissible fade show mb-2`} role="alert">
+        {props.message}
+        <button type="button" className="close" onClick={() => props.onClose()}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    )
   );
 }
 
