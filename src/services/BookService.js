8f429fb7b6ad725606d6f5de35da91002bb00190
diogo-santos <diogoffs@outlook.com>
2020-09-10 00:19:24 +0100

    add function handle error to services

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,15 +1,22 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export async function getBooks(pageNumber, pageSize, sortBy) {
-  const url = `${API_URL}?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}`
+  const url = `${API_URL}?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}`;
   const response = await fetch(url);
   
-  return response;
+  return handleErrors(response);
 }
 
 export async function deleteBook(id) {
   const url = `${API_URL}/${id}`;
   const response = await fetch(url, { method: 'DELETE' });
 
+  return handleErrors(response);
+}
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
   return response;
 }
